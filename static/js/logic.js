d3ee112e13d11116141f1fce1d5dcff5ecfc1685
Alexis Perumal <>
2020-03-29 10:15:19 -0700

    Hello, World leaflet app, from online tutorial.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,1 +1,15 @@
 // Alexis Perumal, 3/28/20
+// Code leveraged from the leaflet Tutorial at https://leafletjs.com/examples/quick-start/
+
+// Create a map object
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+// Setup the Base Map, the tile layer
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+}).addTo(mymap);
